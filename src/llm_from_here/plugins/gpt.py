1c67e3ec3c3f3c4a9121fa5b93cadfbc5e46fa77
Matt Bass <matthewbass@gmail.com>
2023-11-10 09:18:30 -0500

    Update gpt.py to revert api upgrade

--- src/llm_from_here/plugins/gpt.py
+++ src/llm_from_here/plugins/gpt.py
@@ -47,9 +47,9 @@ class ChatApp:
     def chat(self, message, strip_quotes=False, tries=5, delay=2, backoff=2):
         @retry(
             (
-                openai.RateLimitError,
-                openai.AuthenticationError,
-                openai.APIError,
+                openai.error.RateLimitError,
+                openai.error.AuthenticationError,
+                openai.error.APIError,
             ),
             tries=tries,
             delay=delay,
