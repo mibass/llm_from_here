8eb32cb5f746102f82827dbca46767ca9a3397f5
Matt Bass <matthewbass@gmail.com>
2024-04-06 16:46:40 -0400

    Update setup.py
    Pin pyaml-include

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'matplotlib',
         'pandas',
         'freesound-python @ git+https://github.com/MTG/freesound-python.git#egg=freesound',
-        'pyyaml-include',
+        'pyyaml-include<2',
         'pytest',
         'ytmusicapi'
     ],
